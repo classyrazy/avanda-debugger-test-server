491d4b4a3ce23fc4b6856b3638d435381b92ad25
Wharley01 <hackerslord96@gmail.com>
2021-10-23 18:23:06 +0100

    removed unneeded files

--- app/models/User.ts
+++
@@ -1,9 +0,0 @@
-import {Column,Model} from "@avanda/orm";
-export default class User extends Model{
-    @Column.text()
-    full_name?:string;
-
-    @Column.int()
-    no_of_posts?:number = 0
-
-}
